04f8e7ed05d416ae02ba08ab47e961880816479c
Alex <amhughes@g.cofc.edu>
2016-03-22 16:26:53 -0400

    Moved loop so as to not re-read the csv file every single loop through

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -6,27 +6,26 @@
 (def categories (atom []))
 
 (defn -main []
-  (loop []
-    (println "Pick a number between 1 and 6. Type q to quit.")
-    (let [input (read-line)]
-      (when (not= input "q")
-        (let [purchases (slurp "purchases.csv")
-              purchases (str/split-lines purchases)
-              purchases (map (fn [line]
-                              (str/split line #","))
-                          purchases)
-              header (first purchases)
-              purchases (rest purchases)
-              purchases (map (fn [line]
-                               (apply hash-map (interleave header line)))
-                          purchases)
-              purchases (walk/keywordize-keys purchases)
-              _ (when (empty? @categories) (reset! categories (vec (set (map :category purchases)))))  
-              cat-choice (get @categories (- (Integer/valueOf input) 1))
-              purchases (filter (fn [line]
-                                 (= (:category line) cat-choice))
-                         purchases)]
-          
-          (spit (str cat-choice "_purchases.edn") (pr-str purchases))
-          (println purchases)
-          (recur))))))
+  (let [purchases (slurp "purchases.csv")
+            purchases (str/split-lines purchases)
+            purchases (map (fn [line]
+                            (str/split line #","))
+                        purchases)
+            header (first purchases)
+            purchases (rest purchases)
+            purchases (map (fn [line]
+                             (apply hash-map (interleave header line)))
+                        purchases)
+            purchases (walk/keywordize-keys purchases)
+            _ (when (empty? @categories) (reset! categories (vec (set (map :category purchases)))))];when makes sure the atom isn't poplulated every loop through, but then i moved this out of the loop, left it here for reference  
+    (loop []
+      (println "Pick a number between 1 and 6. Type q to quit.")
+      (let [input (read-line)]
+          (when (not= input "q")
+            (let [cat-choice (get @categories (- (Integer/valueOf input) 1))
+                  purchases (filter (fn [line]
+                                     (= (:category line) cat-choice))
+                                   purchases)]
+              (spit (str cat-choice "_purchases.edn") (pr-str purchases))
+              (println purchases)
+              (recur)))))))
